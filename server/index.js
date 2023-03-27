c6e1dfdd56d0b6447cc2c1903271c94f1e5350b5
Veervijay <virajrana123@gmail.com>
2023-03-27 19:12:42 -0400

    Admin frontend backend

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import clientRoutes from "./routes/client.js";
 import generalRoutes from "./routes/general.js";
-import managementRoutes from "./routes/managment.js";
+import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
 //  Data Imports
@@ -39,7 +39,7 @@ app.use(cors());
 /* ROUTES */
 app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
-app.use("/managment", managementRoutes);
+app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
 
 /* MONGOOSE SETUP*/
